944884b1f0a0366e18fa8e0d3f4b2f44466981a0
Alexandro Henrique Loch <alexsctj@gmail.com>
2020-02-03 04:57:53 -0300

    Modified 013 strip Comments src.js

---
+++ src/013 strip Comments src.js
@@ -0,0 +1,9 @@
+//https://www.codewars.com/kata/51c8e37cee245da6b40000bd/train/javascript
+
+function solution(input, markers) {
+  
+};
+
+
+//checkComments("apples, plums % and bananas\npears\noranges !applesauce", ["%", "!"], "apples, plums\npears\noranges")
+//checkComments("Q @b\nu\ne -e f g", ["@", "-"], "Q\nu\ne")
