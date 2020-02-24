05aae658edd34c8b375174475e8245e59889c7ae
Alexandro Henrique Loch <alexsctj@gmail.com>
2020-02-24 19:56:31 -0300

    Modified 037 find Odd src.js

---
+++ src/037 find Odd src.js
@@ -0,0 +1,20 @@
+function findOdd(numb) {
+  numb.sort(function (a, b) {
+    return a - b;
+  });
+  let resposta;
+  let i = 0;
+  do {
+    i++;
+    if (numb[i] === numb[i - 1]) {
+      numb[i] = null;
+      numb[i - 1] = null;
+    } else if (numb[i - 1] !== null) {
+      resposta = numb[i - 1];
+    }
+  } while (i < numb.length);
+  return resposta;
+}
+const arr = [1,2,2,3,3,5,3,3,3,2,2,1];
+
+console.log(findOdd(arr));
